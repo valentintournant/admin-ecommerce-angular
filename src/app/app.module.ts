9dfcc13b67f814666f63336d4c840d8d398ffa38
Valentin T <valentin.tournant@gmail.com>
2021-08-04 11:13:51 +0200

    create component, models and service for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeleteProductModalComponent } from './components/delete-product-modal/delete-product-modal.component';
+import { AddOrEditProductModalComponent } from './components/add-or-edit-product-modal/add-or-edit-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DeleteProductModalComponent,
+    AddOrEditProductModalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-or-edit-product-modal/add-or-edit-product-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-or-edit-product-modal',
+  templateUrl: './add-or-edit-product-modal.component.html',
+  styleUrls: ['./add-or-edit-product-modal.component.css']
+})
+export class AddOrEditProductModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
